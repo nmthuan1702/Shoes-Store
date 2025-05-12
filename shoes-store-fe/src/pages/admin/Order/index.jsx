8a31c901a6f9ab85caf39d6e9eb89c0b0a0ec590
nmthuan1702 <thuannguyen35753@gmail.com>
2025-05-12 14:17:47 -0700

    import

--- shoes-store-fe/src/pages/admin/Order/index.jsx
+++ shoes-store-fe/src/pages/admin/Order/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import adminOrderApi from "../../../api/admin/adminOrderApi";
 import ViewOrderModal from "./ViewOrderModal";
 import DeleteOrderModal from "./DeleteOrderModal";
 import { toast } from "react-toastify";
